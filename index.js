20d4f91a433af57aa46d79c44dbe305a34fd51b6
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-12-07 14:24:03 +0200

    add objects

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/ORybka/a-tiny-JS-world
+   Web app: https://orybka.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const man = {
+   species: 'human',
+   name: 'John',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'Hey! What\'s up?',
+   friends: ['Mary', 'Marley', 'Tisha']
+};
 
+const woman = {
+   species: 'human',
+   name: 'Mary',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: 'Hey there!',
+   friends: ['John', 'Marley', 'Tisha']
+};
+
+const dog = {
+   species: 'dog',
+   name: 'Marley',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'woof-woof!',
+   friends: ['John', 'Mary', 'Selina']
+};
+
+const cat = {
+   species: 'cat',
+   name: 'Tisha',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'meow-purr!',
+   friends: []
+};
+
+const catWoman = {
+   species: 'catWoman',
+   name: 'Selina',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+   friends: ['Tisha']
+};
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const objects = [man, woman, dog, cat, catWoman];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+objects.forEach(el => {
+   const objFriends = el.friends.join(', ')
+   const objKeys = [`${el.species}`, `<strong>${el.name}</strong>`, `${el.gender}`, `${el.legs}`, `${el.hands}`, `<em>${el.saying}</em>`, objFriends];
+   print(objKeys.join('; '), 'div')
+});
